626aac9321ca818867d8292e9ac05ad5e6ec23d2
Shivam <palshiv437@gmail.com>
2025-07-31 22:28:20 +0530

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,18 @@
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = { Name = "main-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-south-1a"
+  tags = { Name = "public-subnet" }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+  tags = { Name = "main-igw" }
+}
